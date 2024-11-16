0d88be899a0d70519554c53ff1d7079b30893b09
senja <senja.vuoristo@aalto.fi>
2024-11-16 20:58:04 +0200

    Excercises 6 and 7.

---
+++ Module2/As6/06dice.js
@@ -0,0 +1,15 @@
+function rollDice() {
+  return Math.floor(Math.random() * 6 + 1)
+}
+
+
+const ul = document.createElement('ul')
+let rolled;
+do {
+  rolled = rollDice()
+  const li = document.createElement('li')
+  li.textContent = rolled
+  ul.appendChild(li)
+} while (rolled !== 6)
+
+document.querySelector("#here").appendChild(ul)
